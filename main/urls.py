5df2973ff55f3b513bea16af2889033c7b615228
kaachiii <kaachi579@gmail.com>
2024-09-25 15:55:52 +0700

    Tutorial 4 menambahkan CSS

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from main.views import show_main, create_mood_entry, show_xml
 from django.urls import path
 from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
 from main.views import register
-from main.views import login_user, logout_user
+from main.views import login_user, logout_user, edit_mood, delete_mood
 
 app_name = 'main'
 
@@ -15,5 +15,7 @@ urlpatterns = [
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout')
+    path('logout/', logout_user, name='logout'),
+    path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
+    path('delete/<uuid:id>', delete_mood, name='delete_mood'), # sesuaikan dengan nama fungsi yang dibuat
 ]
